34a8d3397a146988691edbebdb54a605baaccd20
mayhemoffice <mayhemus@yandex.ru>
2023-08-23 14:21:50 +0300

    23.08

---
+++ trell.js
@@ -0,0 +1,42 @@
+const tryaction = {
+  id: "64ca3b16b93c2df6250dad95",
+  idMemberCreator: "6204df00bd9be6611f34f464",
+  data: {
+    card: {
+      desc: "стачек 101 5 парадная\nэтажные 1-15 этаж 1 комплект\n89626960212",
+      id: "64ca30953946a84f6dd941f6",
+      name: "стачек 101",
+      idShort: 1333,
+      shortLink: "5TIwqBbn",
+    },
+    old: { desc: "стачек 101 5 парадная\nэтажные 1-15 этаж 1 комплект" },
+    board: {
+      id: "5bb74741b3108877c3b43fb5",
+      name: "КОПТЕВ СЕРГЕЙ",
+      shortLink: "wEUV5uBr",
+    },
+    list: { id: "5d5a5cdb2e1870047b744f6a", name: "Юго-запад" },
+  },
+  appCreator: null,
+  type: "updateCard",
+  date: "2023-08-02T11:16:38.458Z",
+  limits: null,
+  memberCreator: {
+    id: "6204df00bd9be6611f34f464",
+    activityBlocked: false,
+    avatarHash: "d03ede17f031918158d4883623d7e6bd",
+    avatarUrl:
+      "https://trello-members.s3.amazonaws.com/6204df00bd9be6611f34f464/d03ede17f031918158d4883623d7e6bd",
+    fullName: "Владимир",
+    idMemberReferrer: null,
+    initials: "В",
+    nonPublic: { fullName: "Владимир", avatarHash: null },
+    nonPublicAvailable: true,
+    username: "user14367368",
+  },
+};
+
+const name = tryaction.data.card.name;
+const disc = tryaction.data.card.desc;
+const date = tryaction.date;
+const list = tryaction.data.list.name;
