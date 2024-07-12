37541e32f77ba0983927680bda3457abb3eee4ce
mayhemoffice <mayhemus@yandex.ru>
2024-07-12 15:01:22 +0300

    letter color fix2

--- src/Components/AdressTable/AdressLetter.jsx
+++ src/Components/AdressTable/AdressLetter.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-function AdressLetter({ children }) {
-  const color = children ==="_"? "#312783" : "white"
-  return <div style={{color: color}}>{children}</div>;
+function AdressLetter({children}) {
+  const letter = children === " " ? "_" : children
+  const color = letter === "_" ? "#312783" : "white"
+  return <div style={{color: color}}>{letter}</div>;
 }
 
 export default AdressLetter;
